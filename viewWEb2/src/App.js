1216b4917beb76fad56d5db7575f017f034bee11
thanh-do28 <96042778+thanh-do28@users.noreply.github.com>
2023-06-27 19:03:42 +0700

    project init

---
+++ viewWEb2/src/App.js
@@ -0,0 +1,12 @@
+import Homepage from "./homepage/Homepage";
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <Homepage></Homepage>
+    </div>
+  );
+}
+
+export default App;
